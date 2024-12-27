f0b5a25da71446a23567eeca231e254a979970a8
takuya <est2mzd@gmail.com>
2024-12-28 00:45:55 +0900

    refactor

--- catkin_ws2_noetic/00_create_cmake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p ~/catkin_ws2_noetic/src
-cd ~/catkin_ws2_noetic/src
-catkin_init_workspace
--- catkin_ws2_noetic/81_copy_launch_file_ekf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cd ~/catkin_ws2_noetic/
-cp ./robot_pose_ekf.launch ./src/robot_pose_ekf/
